eb3e8a07d413bf2b2c7305a4338129ba51edd257
akmalr02 <akmalrahmansyah02@gmail.com>
2025-08-04 16:10:11 +0700

    force https in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,6 +22,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         DB::listen(function ($query) {
             Log::info("Query: {$query->sql}, Bindings: " . json_encode($query->bindings) . ", Time: {$query->time}ms");
         });
